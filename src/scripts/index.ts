bee58db6ba81ff0d7a4067d83208a1a823392d63
Vladislav Pipko <77760319+Oxxxymirana@users.noreply.github.com>
2021-12-10 09:06:36 +0300

    complete game logic

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -2,9 +2,10 @@ import * as Phaser from "phaser";
 import { StartScene } from "./scenes/StartScene";
 import { GameScene } from "./scenes/GameScene";
 
+
 new Phaser.Game({
     type: Phaser.AUTO,
-    parent: "minesweeper",
+    parent: "battleship",
     width: window.innerWidth,
     height: window.innerHeight,
     backgroundColor: "#F0FFFF",
